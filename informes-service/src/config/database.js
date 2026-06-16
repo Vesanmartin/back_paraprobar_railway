cd5a8775c4565c3aac08c17064339cf5910a6d92
Vesanmartin <ve.sanmartin@duocuc.cl>
2026-06-15 22:46:12 -0400

    actualiza database.js y package-lock.json

--- informes-service/src/config/database.js
+++ informes-service/src/config/database.js
@@ -11,6 +11,8 @@ const pool = mysql.createPool({
   user:               process.env.DB_USER,
   password:           process.env.DB_PASSWORD,
   database:           process.env.DB_NAME,
+  charset:            'utf8mb4',
+  timezone:           '+00:00',
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0
